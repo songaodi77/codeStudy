b262c119a0d6e05d5da579926e2e67348dc44af5
149874787 <149874787@qq.com>
2023-10-10 01:38:54 +0800

    start

--- codeStudy/SqList.cpp
+++ codeStudy/SqList.cpp
@@ -1,1 +1,8 @@
 #include "SqList.h"
+
+
+bool InitList()
+{
+	printf("hello");
+	return false;
+}
--- codeStudy/SqList.h
+++ codeStudy/SqList.h
@@ -1,17 +1,13 @@
 #pragma once
+#include<iostream>
 #define MAXSIZE 100
 #define elemType int
-class SqList
-{
-	typedef struct {
-		elemType *data;
-		int size; // 存储当前链表的长度
-		int maxSize;//存储当前链表所拥有的内存大小
-	}SqList_node;
-	
-	bool InitSqList(SqList_node &L) {
-		// 申请地址空间存储链表
-		elemType *node=malloc(MAXSIZE * sizeof(elemType));
-	}
-};
+
+typedef struct {
+	elemType *data;
+	int size; // 存储当前链表的长度
+	int maxSize;//存储当前链表所拥有的内存大小
+}SqList_node;
+
+bool InitList();
 
